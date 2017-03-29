ab5a03e8a4a4f1d2349a7ca50872465892f56381
광수 <@192.168.10.51>
2017-03-29 20:18:13 +0900

    agde

--- BKFramework/src/main/java/com/bunker/bkframework/working/Working.java
+++
@@ -1,9 +0,0 @@
-package com.bunker.bkframework.working;
-
-import java.util.Map;
-
-import org.json.simple.JSONObject;
-
-public interface Working {
-	public WorkingResult doWork(JSONObject object, Map<String, Object> enviroment);
-}
